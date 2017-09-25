77a1e27d07c312596dabcafc9ec88b11c6f2d738
Andrey Brunetkin <brunetkin@reg.ru>
2017-09-25 06:48:43 +0500

    base

---
+++ server/network/net.io/net.io-client/index.js
@@ -0,0 +1,186 @@
+// Our namespace
+var NetIo = {};
+
+/**
+ * Define the debug options object.
+ * @type {Object}
+ * @private
+ */
+NetIo._debug = {
+	_enabled: true,
+	_node: typeof(module) !== 'undefined' && typeof(module.exports) !== 'undefined',
+	_level: ['log', 'warning', 'error'],
+	_stacks: false,
+	_throwErrors: true,
+	_trace: {
+		setup: false,
+		enabled: false,
+		match: ''
+	},
+	enabled: function (val) {
+		if (val !== undefined) {
+			this._enabled = val;
+			return this;
+		}
+
+		return this._enabled;
+	}
+};
+
+// https://github.com/pieroxy/lz-string/blob/master/libs/lz-string.min.js
+//NetIo._compressor=function(){function o(o,r){if(!t[o]){t[o]={};for(var n=0;n<o.length;n++)t[o][o.charAt(n)]=n}return t[o][r]}var r=String.fromCharCode,n="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",e="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+-$",t={},i={compressToBase64:function(o){if(null==o)return"";var r=i._compress(o,6,function(o){return n.charAt(o)});switch(r.length%4){default:case 0:return r;case 1:return r+"===";case 2:return r+"==";case 3:return r+"="}},decompressFromBase64:function(r){return null==r?"":""==r?null:i._decompress(r.length,32,function(e){return o(n,r.charAt(e))})},compressToUTF16:function(o){return null==o?"":i._compress(o,15,function(o){return r(o+32)})+" "},decompressFromUTF16:function(o){return null==o?"":""==o?null:i._decompress(o.length,16384,function(r){return o.charCodeAt(r)-32})},compressToUint8Array:function(o){for(var r=i.compress(o),n=new Uint8Array(2*r.length),e=0,t=r.length;t>e;e++){var s=r.charCodeAt(e);n[2*e]=s>>>8,n[2*e+1]=s%256}return n},decompressFromUint8Array:function(o){if(null===o||void 0===o)return i.decompress(o);for(var n=new Array(o.length/2),e=0,t=n.length;t>e;e++)n[e]=256*o[2*e]+o[2*e+1];var s=[];return n.forEach(function(o){s.push(r(o))}),i.decompress(s.join(""))},compressToEncodedURIComponent:function(o){return null==o?"":i._compress(o,6,function(o){return e.charAt(o)})},decompressFromEncodedURIComponent:function(r){return null==r?"":""==r?null:(r=r.replace(/ /g,"+"),i._decompress(r.length,32,function(n){return o(e,r.charAt(n))}))},compress:function(o){return i._compress(o,16,function(o){return r(o)})},_compress:function(o,r,n){if(null==o)return"";var e,t,i,s={},p={},u="",c="",a="",l=2,f=3,h=2,d=[],m=0,v=0;for(i=0;i<o.length;i+=1)if(u=o.charAt(i),Object.prototype.hasOwnProperty.call(s,u)||(s[u]=f++,p[u]=!0),c=a+u,Object.prototype.hasOwnProperty.call(s,c))a=c;else{if(Object.prototype.hasOwnProperty.call(p,a)){if(a.charCodeAt(0)<256){for(e=0;h>e;e++)m<<=1,v==r-1?(v=0,d.push(n(m)),m=0):v++;for(t=a.charCodeAt(0),e=0;8>e;e++)m=m<<1|1&t,v==r-1?(v=0,d.push(n(m)),m=0):v++,t>>=1}else{for(t=1,e=0;h>e;e++)m=m<<1|t,v==r-1?(v=0,d.push(n(m)),m=0):v++,t=0;for(t=a.charCodeAt(0),e=0;16>e;e++)m=m<<1|1&t,v==r-1?(v=0,d.push(n(m)),m=0):v++,t>>=1}l--,0==l&&(l=Math.pow(2,h),h++),delete p[a]}else for(t=s[a],e=0;h>e;e++)m=m<<1|1&t,v==r-1?(v=0,d.push(n(m)),m=0):v++,t>>=1;l--,0==l&&(l=Math.pow(2,h),h++),s[c]=f++,a=String(u)}if(""!==a){if(Object.prototype.hasOwnProperty.call(p,a)){if(a.charCodeAt(0)<256){for(e=0;h>e;e++)m<<=1,v==r-1?(v=0,d.push(n(m)),m=0):v++;for(t=a.charCodeAt(0),e=0;8>e;e++)m=m<<1|1&t,v==r-1?(v=0,d.push(n(m)),m=0):v++,t>>=1}else{for(t=1,e=0;h>e;e++)m=m<<1|t,v==r-1?(v=0,d.push(n(m)),m=0):v++,t=0;for(t=a.charCodeAt(0),e=0;16>e;e++)m=m<<1|1&t,v==r-1?(v=0,d.push(n(m)),m=0):v++,t>>=1}l--,0==l&&(l=Math.pow(2,h),h++),delete p[a]}else for(t=s[a],e=0;h>e;e++)m=m<<1|1&t,v==r-1?(v=0,d.push(n(m)),m=0):v++,t>>=1;l--,0==l&&(l=Math.pow(2,h),h++)}for(t=2,e=0;h>e;e++)m=m<<1|1&t,v==r-1?(v=0,d.push(n(m)),m=0):v++,t>>=1;for(;;){if(m<<=1,v==r-1){d.push(n(m));break}v++}return d.join("")},decompress:function(o){return null==o?"":""==o?null:i._decompress(o.length,32768,function(r){return o.charCodeAt(r)})},_decompress:function(o,n,e){var t,i,s,p,u,c,a,l,f=[],h=4,d=4,m=3,v="",w=[],A={val:e(0),position:n,index:1};for(i=0;3>i;i+=1)f[i]=i;for(p=0,c=Math.pow(2,2),a=1;a!=c;)u=A.val&A.position,A.position>>=1,0==A.position&&(A.position=n,A.val=e(A.index++)),p|=(u>0?1:0)*a,a<<=1;switch(t=p){case 0:for(p=0,c=Math.pow(2,8),a=1;a!=c;)u=A.val&A.position,A.position>>=1,0==A.position&&(A.position=n,A.val=e(A.index++)),p|=(u>0?1:0)*a,a<<=1;l=r(p);break;case 1:for(p=0,c=Math.pow(2,16),a=1;a!=c;)u=A.val&A.position,A.position>>=1,0==A.position&&(A.position=n,A.val=e(A.index++)),p|=(u>0?1:0)*a,a<<=1;l=r(p);break;case 2:return""}for(f[3]=l,s=l,w.push(l);;){if(A.index>o)return"";for(p=0,c=Math.pow(2,m),a=1;a!=c;)u=A.val&A.position,A.position>>=1,0==A.position&&(A.position=n,A.val=e(A.index++)),p|=(u>0?1:0)*a,a<<=1;switch(l=p){case 0:for(p=0,c=Math.pow(2,8),a=1;a!=c;)u=A.val&A.position,A.position>>=1,0==A.position&&(A.position=n,A.val=e(A.index++)),p|=(u>0?1:0)*a,a<<=1;f[d++]=r(p),l=d-1,h--;break;case 1:for(p=0,c=Math.pow(2,16),a=1;a!=c;)u=A.val&A.position,A.position>>=1,0==A.position&&(A.position=n,A.val=e(A.index++)),p|=(u>0?1:0)*a,a<<=1;f[d++]=r(p),l=d-1,h--;break;case 2:return w.join("")}if(0==h&&(h=Math.pow(2,m),m++),f[l])v=f[l];else{if(l!==d)return null;v=s+s.charAt(0)}w.push(v),f[d++]=s+v.charAt(0),h--,s=v,0==h&&(h=Math.pow(2,m),m++)}}};return i}();"function"==typeof define&&define.amd?define(function(){return LZString}):"undefined"!=typeof module&&null!=module&&(module.exports=LZString);
+
+/**
+ * Define the class system.
+ * @type {*}
+ */
+NetIo.Class = IgeClass;
+
+NetIo.EventingClass = IgeEventingClass;
+
+NetIo.Client = NetIo.EventingClass.extend({
+	classId: 'NetIo.Client',
+
+	init: function (url, options) {
+		this.log('Net.io client starting...');
+		this._options = options || {};
+		this._socket = null;
+		this._state = 0;
+		this._debug = false;
+		this._connectionAttempts = 0;
+
+		// Set some default options
+		if (this._options.connectionRetry === undefined) { this._options.connectionRetry = true; }
+		if (this._options.connectionRetryMax === undefined) { this._options.connectionRetryMax = 10; }
+		if (this._options.reconnect === undefined) { this._options.reconnect = true; }
+
+		// If we were passed a url, connect to it
+		if (url !== undefined) {
+			this.connect(url);
+		}
+	},
+
+	/**
+	 * Gets / sets the debug flag. If set to true, net.io
+	 * will output debug data about every network event as
+	 * it occurs to the console.
+	 * @param {Boolean=} val
+	 * @return {*}
+	 */
+	debug: function (val) {
+		if (val !== undefined) {
+			this._debug = val;
+			return this;
+		}
+
+		return this._debug;
+	},
+
+	connect: function (url) {
+		this.log('Connecting to server at ' + url);
+		var self = this;
+
+		// Set the state to connecting
+		this._state = 1;
+
+		// Replace http:// with ws://
+		url = url.replace('http://', 'ws://');
+
+		// Create new websocket to the url
+		this._socket = new WebSocket(url, 'netio1');
+
+		// Setup event listeners
+		this._socket.onopen = function () { self._onOpen.apply(self, arguments); };
+		this._socket.onmessage = function () { self._onData.apply(self, arguments); };
+		this._socket.onclose = function () { self._onClose.apply(self, arguments); };
+		this._socket.onerror = function () { self._onError.apply(self, arguments); };
+	},
+
+	disconnect: function (reason) {
+		this._socket.close(1000, reason);
+	},
+
+	send: function (data) {
+		this._socket.send(this._encode(data));
+	},
+
+	_onOpen: function () {
+		this._state = 2;
+	},
+
+	_onData: function (data) {
+		// Decode packet and emit message event
+		var packet = this._decode(data.data);
+
+		// Output debug if required
+		if (this._debug) {
+			console.log('Incoming data (event, decoded data):', data, packet);
+		}
+
+		if (packet._netioCmd) {
+			// The packet is a netio command
+			switch (packet._netioCmd) {
+				case 'id':
+					// Store the new id in the socket
+					this.id = packet.data;
+
+					// Now we have an id, set the state to connected
+					this._state = 3;
+
+					// Emit the connect event
+					this.emit('connect', this.id);
+					break;
+
+				case 'close':
+					// The server told us our connection has been closed
+					// so store the reason the server gave us!
+					this._disconnectReason = packet.data;
+					break;
+			}
+		} else {
+			// The packet is normal data
+			this.emit('message', [packet]);
+		}
+	},
+
+	_onClose: function (code, reason, wasClean) {
+		// If we are already connected and have an id...
+		if (this._state === 3) {
+			this._state = 0;
+			this.emit('disconnect', {reason: this._disconnectReason, wasClean: wasClean, code:code});
+		}
+
+		// If we are connected but have no id...
+		if (this._state === 2) {
+			this._state = 0;
+			this.emit('disconnect', {reason: this._disconnectReason, wasClean: wasClean, code:code});
+		}
+
+		// If we were trying to connect...
+		if (this._state === 1) {
+			this._state = 0;
+			this.emit('error', {reason: 'Cannot establish connection, is server running?'});
+		}
+
+		// Remove the last disconnect reason
+		delete this._disconnectReason;
+	},
+
+	_onError: function () {
+		this.log('An error occurred with the net.io socket!', 'error', arguments);
+		this.emit('error', arguments);
+	},
+
+	_encode: function (data) {
+		// console.log('CLIENT::_encode: ', data);
+		// return NetIo._compressor.compress(JSON.stringify(data));
+		return JSON.stringify(data);
+	},
+
+	_decode: function (data) {
+		// var decomp = NetIo._compressor.decompress(data);
+		// console.log('CLIENT::_decode: ', data, '\n', decomp);
+		// return decomp;
+		return JSON.parse(data);
+	}
+});
---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const path              = require('path');
+const os                = require('os');
+const glob              = require('glob');
+const webpack           = require('webpack');
+const UglifyJSPlugin    = require('uglifyjs-webpack-plugin');
+
+var options = {};
+
+
+module.exports = {
+    entry : {
+        export : path.resolve( __dirname, 'app/export.babel.js' ),
+        main   : path.resolve( __dirname, 'app/main.babel.js' ),
+    },
+    output : {
+        path       : path.resolve( __dirname, './dist/' ),
+        publicPath : '/dist/',
+        filename   : '[name].js',
+    },
+    module : { rules : [
+        {
+            test    : /\.js$/,
+            loader  : 'babel-loader',
+            exclude : /node_modules/,
+        },
+    ] },
+    resolve : {
+        unsafeCache : true,
+        modules     : [
+            /*path.resolve('./node_modules'),*/
+        ],
+    },
+    performance : { hints: false },
+    devtool     : '#eval-source-map',
+};
+
+if ( process.env.NODE_ENV === 'production' ) {
+    options.devtool = '#source-map';
+
+  // http://vue-loader.vuejs.org/en/workflow/production.html
+    module.exports.plugins = ( module.exports.plugins || [] ).concat( [
+        new webpack.DefinePlugin( { 'process.env' : {
+            NODE_ENV : `"${process.env.NODE_ENV}"`,
+        } } ),
+        new webpack.optimize.UglifyJsPlugin( { sourceMap: options.devtool && options.devtool.indexOf('source-map') >= 0 } ),
+    ] );
+}
